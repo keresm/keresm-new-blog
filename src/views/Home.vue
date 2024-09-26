4827e835ba817d10b79d8cc288628a0f9cfbe31c
Aiyyna <49439078+keresm@users.noreply.github.com>
2024-09-26 21:47:26 +0900

    ner version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@ import store from '../store.js'
 </script>
 
 <template>
-    <div class="Home">
+    <div class="Home container mx-auto">
         <main clas="container mx-auto">
             <div class="PostItem border border-slate-200 mb-4 p-4 rounded-lg" v-for="item, itemIndex in store.posts" :key="itemIndex" @click="$router.push(`/post/${item.id}`)">
                 <h1 class="text-slate-900 text-3xl font-bold mb-4"> {{ item.title }}
